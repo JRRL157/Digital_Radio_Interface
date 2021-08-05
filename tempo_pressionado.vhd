74a2bb5bce5f30a958bed8a3f5cd442f68786f6f
JRRL157 <64756147+JRRL157@users.noreply.github.com>
2021-08-05 19:09:52 -0300

    Tempo pressionado

---
+++ tempo_pressionado.vhd
@@ -0,0 +1,38 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+entity tempo_pressionado IS
+	PORT(
+		clk : IN std_logic;
+		btn1 : IN std_logic;
+		btn2 : IN std_logic;
+		res : OUT std_logic;
+		led_cont : OUT std_logic
+	);
+END tempo_pressionado;
+
+ARCHITECTURE tempo OF tempo_pressionado IS
+	SIGNAL contador : integer := 0;
+	SIGNAL resposta : std_logic := '0';
+BEGIN
+	
+	process(clk) is
+	begin
+		IF (rising_edge(clk)) THEN
+			IF (btn1 = '0' AND btn2 = '0') THEN
+				contador <= contador + 1;
+			ELSE
+				contador <= 0;
+			END IF;
+		END IF;		
+		
+		IF (contador >= 100_000_000) THEN
+			resposta <= '1';
+		ELSE
+			resposta <= '0';
+		END IF;
+	END PROCESS;
+
+	res <= resposta;
+
+END tempo;
